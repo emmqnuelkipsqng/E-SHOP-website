c2c8120ac84ff906b8276bf7e7999c40da4d7663
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-01-27 20:59:54 +0300

    Add POST Request

--- src/App.js
+++ src/App.js
@@ -11,12 +11,24 @@ export default function App() {
     });
   }, []);
 
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Hello World!",
+        body: "This is a new post",
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
   if (!post) return null;
 
   return (
     <div>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
+      <button onClick={createPost}>Create Post</button>
     </div>
   );
 }
